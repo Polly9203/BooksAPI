7bd12a82274488611879d25a959290bd49f297c7
Polly <apollinaria9203@yandex.by>
2021-08-13 15:53:13 +0300

    connection string to appsettings.json

--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -14,14 +15,29 @@ namespace BooksAPI
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            string con = "Server=(localdb)\\mssqllocaldb;Database=booksdbstore;Trusted_Connection=True;";
-            // устанавливаем контекст данных
-            services.AddDbContext<BooksContext>(options => options.UseSqlServer(con));
+
+            services.AddDbContext<BooksContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
             services.AddControllers(); // используем контроллеры без представлений
         }
+        //public void ConfigureServices(IServiceCollection services)
+        //{
+        //    string con = "Server=(localdb)\\mssqllocaldb;Database=booksdbstore;Trusted_Connection=True;";
+        //    // устанавливаем контекст данных
+        //    services.AddDbContext<BooksContext>(options => options.UseSqlServer(con));
+
+        //    services.AddControllers(); // используем контроллеры без представлений
+        //}
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
