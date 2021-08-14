86ba8f4c5976a9c857fb4d3b241fbaaf2a72dcb8
Polly <apollinaria9203@yandex.by>
2021-08-14 20:04:25 +0300

    Almost integration

---
+++ Controllers/LoveController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BooksAPI.Models;
+
+namespace BooksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoveController : ControllerBase
+    {
+        [HttpGet]
+        async Task<ActionResult<Answer>> GetLoveStats(string fname, string sname)
+        {
+            if (fname == null || sname == null)
+            {
+                return BadRequest();
+            }
+
+            var client = new RestClient("https://love-calculator.p.rapidapi.com/getPercentage?fname=" + fname + "&sname=" + sname);
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("x-rapidapi-key", "{x-rapidapi-key}");
+            request.AddHeader("x-rapidapi-host", "love-calculator.p.rapidapi.com");
+            IRestResponse response = client.Execute(request);
+            Answer answer = JsonConvert.DeserializeObject<Answer>(response.Content);
+
+            return new ObjectResult(answer);
+        }
+    } 
+}
---
+++ Models/Answer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksAPI.Models
+{
+    public class Answer
+    {
+        string fname { get; set; }
+        string sname { get; set; }
+        string percentage { get; set; }
+        string result { get; set; }
+    }
+}
